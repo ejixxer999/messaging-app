e57418c3f166e46cf4b19e302c0befe44c82d2f9
ejixxer999 <lehman.ethan2@gmail.com>
2025-05-27 18:02:17 -0400

    added typing bubbles

--- dist/src/app/page.jsx
+++ dist/src/app/page.jsx
@@ -15,7 +15,9 @@ var secretKey = process.env.SECRET_KEY || 'default-secret-key'; // Use a fallbac
 export default function Home() {
     var _a = useState(''), message = _a[0], setMessage = _a[1];
     var _b = useState([]), messages = _b[0], setMessages = _b[1];
+    var _c = useState(false), isTyping = _c[0], setIsTyping = _c[1];
     var socketRef = useRef(null); // Initialize useRef
+    var typingTimeoutRef = useRef(null);
     useEffect(function () {
         socketRef.current = io();
         socketRef.current.on('chat message', function (encryptedMsg) {
@@ -40,13 +42,34 @@ export default function Home() {
             setMessage('');
         }
     };
-    return (<div>
-            <ul id="messages">
-                {messages.map(function (msg, index) { return (<li key={index}>{msg.content}</li>); })}
+    var handleTyping = function () {
+        setIsTyping(true);
+        if (typingTimeoutRef.current)
+            clearTimeout(typingTimeoutRef.current);
+        typingTimeoutRef.current = setTimeout(function () { return setIsTyping(false); }, 2000);
+    };
+    var stopTyping = function () {
+        setIsTyping(false);
+    };
+    return (<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+            <div className='w-full max-w-md p-4 bh-white rounded-lg shadow-lg'>
+                <div className='message-container'>
+            <ul id="messages" className='space-y-2 rounded-md'>
+                {messages.map(function (msg, index) { return (<li key={index} className='px-4 py-2 bg-gray-200 rounded-md'>{msg.content}</li>); })}
             </ul>
-            <form onSubmit={sendMessage}>
-                <input id="input" autoComplete="off" value={message} onChange={function (e) { return setMessage(e.target.value); }}/>
-                <button>Send</button>
+                </div>
+            <form onSubmit={sendMessage} className='flex mt-4'>
+                <input id="input" autoComplete="off" value={message} onChange={function (e) { return setMessage(e.target.value); }} className="input-field" placeholder="Write your message" onKeyUp={handleTyping} onBlur={stopTyping}/>
+                <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none
+                 focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2
+                  dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700
+                   dark:border-gray-700">Send</button>
+                   {isTyping && (<div className="typing-indicator">
+                        <div className="typing-dot"></div>
+                        <div className="typing-dot"></div>
+                        <div className="typing-dot"></div>
+                    </div>)}
             </form>
+            </div>
         </div>);
 }
